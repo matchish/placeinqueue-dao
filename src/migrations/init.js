4592d98a75c0f97862180d5f33f0aac3ddf43250
Sergey Shliakhov <ss@applemint.eu>
2019-01-22 11:19:41 +0200

    Catch errors

--- src/migrations/init.js
+++ src/migrations/init.js
@@ -10,119 +10,123 @@ let dynamodb = new AWS.DynamoDB();
 
 (async () => {
 
-    await new Promise((resolve, reject) => {
-        let params = {
-            "TableName": "Queues",
-            "KeySchema": [
-                {"AttributeName": "id", "KeyType": "HASH"},
-            ],
-            "AttributeDefinitions": [
-                {"AttributeName": "id", "AttributeType": "S"},
-            ],
-            "ProvisionedThroughput": {
-                "ReadCapacityUnits": 5,
-                "WriteCapacityUnits": 5
-            }
-        }
-        dynamodb.createTable(params, function (err, data) {
-            if (err) {
-                console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-                reject();
-            } else {
-                resolve();
-                console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
-            }
-        });
-    });
-
-    await new Promise((resolve, reject) => {
-        let params = {
-            "TableName": "Places",
-            "KeySchema": [
-                {"AttributeName": "uid", "KeyType": "HASH"},
-            ],
-            "AttributeDefinitions": [
-                {"AttributeName": "uid", "AttributeType": "S"},
-                {"AttributeName": "queue_id", "AttributeType": "S"},
-                {"AttributeName": "sort", "AttributeType": "N"},
-            ],
-            "ProvisionedThroughput": {
-                "ReadCapacityUnits": 5,
-                "WriteCapacityUnits": 5
-            },
-            "GlobalSecondaryIndexes": [{
-                "IndexName": "FirstInQueue",
+    try {
+        await new Promise((resolve, reject) => {
+            let params = {
+                "TableName": "Queues",
                 "KeySchema": [
-                    {
-                        "AttributeName": "queue_id",
-                        "KeyType": "HASH"
-                    },
-                    {
-                        "AttributeName": "sort",
-                        "KeyType": "RANGE"
-                    }
+                    {"AttributeName": "id", "KeyType": "HASH"},
                 ],
-                "Projection": {
-                    "ProjectionType": "ALL"
-                },
+                "AttributeDefinitions": [
+                    {"AttributeName": "id", "AttributeType": "S"},
+                ],
+                "ProvisionedThroughput": {
+                    "ReadCapacityUnits": 5,
+                    "WriteCapacityUnits": 5
+                }
+            }
+            dynamodb.createTable(params, function (err, data) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                    console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+                }
+            });
+        });
+    } catch (e) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(e, null, 2));
+    }
+
+    try {
+        await new Promise((resolve, reject) => {
+            let params = {
+                "TableName": "Places",
+                "KeySchema": [
+                    {"AttributeName": "uid", "KeyType": "HASH"},
+                ],
+                "AttributeDefinitions": [
+                    {"AttributeName": "uid", "AttributeType": "S"},
+                    {"AttributeName": "queue_id", "AttributeType": "S"},
+                    {"AttributeName": "sort", "AttributeType": "N"},
+                ],
                 "ProvisionedThroughput": {
                     "ReadCapacityUnits": 5,
                     "WriteCapacityUnits": 5
                 },
-            }]
-        }
-
-        dynamodb.createTable(params, function (err, data) {
-            if (err) {
-                console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-                reject();
-            } else {
-                console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
-                resolve();
-                let params = {
-                    TableName: 'Places',
-                    TimeToLiveSpecification: {
-                        AttributeName: 'expires_at',
-                        Enabled: true
-                    }
-                };
-
-                dynamodb.updateTimeToLive(params, function (err, data) {
-                    if (err) console.log(err, err.stack); // an error occurred
-                    else console.log(data);           // successful response
-                });
+                "GlobalSecondaryIndexes": [{
+                    "IndexName": "FirstInQueue",
+                    "KeySchema": [
+                        {
+                            "AttributeName": "queue_id",
+                            "KeyType": "HASH"
+                        },
+                        {
+                            "AttributeName": "sort",
+                            "KeyType": "RANGE"
+                        }
+                    ],
+                    "Projection": {
+                        "ProjectionType": "ALL"
+                    },
+                    "ProvisionedThroughput": {
+                        "ReadCapacityUnits": 5,
+                        "WriteCapacityUnits": 5
+                    },
+                }]
             }
+
+            dynamodb.createTable(params, function (err, data) {
+                if (err) {
+                    reject(err);
+                } else {
+                    console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+                    resolve();
+                    let params = {
+                        TableName: 'Places',
+                        TimeToLiveSpecification: {
+                            AttributeName: 'expires_at',
+                            Enabled: true
+                        }
+                    };
+                }
+            });
         });
-    });
+    } catch (e) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(e, null, 2));
+    }
 
-    await new Promise((resolve, reject) => {
+    try {
+        await new Promise((resolve, reject) => {
 
-        let params = {
-            TableName: 'Places'
-        };
+            let params = {
+                TableName: 'Places'
+            };
 
-        dynamodb.waitFor('tableExists', params, (err, data) => {
-            if (err) console.log(err, err.stack);
-            else{
-                let params = {
-                    TableName: 'Places',
-                    TimeToLiveSpecification: {
-                        AttributeName: 'expires_at',
-                        Enabled: true
-                    }
-                };
-                dynamodb.updateTimeToLive(params, function (err, data) {
-                    if (err) {
-                        console.log(err, err.stack);
-                        reject();
-                    }
-                    else {
-                        console.log(data);
-                        resolve();
-                    }
-                });
-            }
+            dynamodb.waitFor('tableExists', params, (err, data) => {
+                if (err) console.log(err, err.stack);
+                else {
+                    let params = {
+                        TableName: 'Places',
+                        TimeToLiveSpecification: {
+                            AttributeName: 'expires_at',
+                            Enabled: true
+                        }
+                    };
+                    dynamodb.updateTimeToLive(params, function (err, data) {
+                        if (err) {
+                            reject(err);
+                        }
+                        else {
+                            console.log(data);
+                            resolve();
+                        }
+                    });
+                }
+            });
+
         });
-
-    });
+    } catch (e) {
+        console.log(e, err.stack);
+    }
 })();
